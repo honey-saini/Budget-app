310c4751783b2f802cefd4420977a9cc63598677
hs16058922 <54467410+hs16058922@users.noreply.github.com>
2020-07-03 17:27:53 +0530

    setup the user data

---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Route, Redirect } from 'react-router-dom'
+
+
+export const PublicRoute = ({
+    isAutherised,
+    component: Component,
+    ...rest
+   }) => (
+        <Route {...rest} component={(props) =>
+            isAutherised ? (
+                <Redirect to='/dashboard' />
+            ) : (
+                <Component {...props} />
+                )
+        } />
+    );
+
+const mapStateToProps = (state) => ({
+    isAutherised: !!state.auth.uid
+})
+
+export default connect(mapStateToProps)(PublicRoute)
