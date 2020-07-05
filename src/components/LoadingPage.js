041371e7867ea525f48615b951646c030cac10e3
hs16058922 <54467410+hs16058922@users.noreply.github.com>
2020-07-05 18:01:28 +0530

    styles deployment

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingPage = () => (
+    <div className='loader'>
+     <img  className='loader-image' src="/images/g0R5.gif"/>
+    </div>
+)
+
+export default LoadingPage;
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+   <div className="box-layout__box">
+   <h1 className="box-layout__title">Expensify-app</h1>
+   <p>IT's time to leave your expenses</p>
+   <button className="btn" onClick={startLogin}>Login with Google</button>
+   </div>
   </div>
 );
 
